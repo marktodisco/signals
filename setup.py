ff7d8b2cca8d6188493b15443de49971830ef24c
Mark Todisco <marktodisco@gmail.com>
2020-10-12 00:06:50 -0400

    Reduced setup file to barebones.

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@ from setuptools import setup, find_packages
 
 setup(
     name='signals',
-    version='0.0.1',
-    packages=find_packages(),
-    install_requires=['numpy', 'matplotlib'],
-    python_requires='>=3.7, <3.8'
+    packages=find_packages()
 )
